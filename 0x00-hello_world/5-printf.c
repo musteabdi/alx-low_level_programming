44ff885eab504e0265c2133d3a3efa1609759c83
musteabdi <musteabdi@hotmail.com>
2022-12-20 15:49:23 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /**
-* main - prints out a string via printf
-* Description: Program that prints out the string s followed by a new line
-* Return: int 0 when done and no errors
-*/
-
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
