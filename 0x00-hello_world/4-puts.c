85166661478c4ba244b50d6a7af6c793c4b45cbb
musteabdi <musteabdi@hotmail.com>
2022-12-26 15:37:30 -0800

    task5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
 /**
-* main - prints out a string via puts
-* Description: Program that prints out the string s followed by a new line
-* Return: int 0 when done and no errors
-*/
-
+ * main - prints the phrase "Programming is like building a multilingual puzzle"
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
