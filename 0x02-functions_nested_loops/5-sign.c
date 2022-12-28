fa8ebf4c022253ab77dc713a8ccc43603435b20a
musteabdi <musteabdi@hotmail.com>
2022-12-28 05:03:48 -0800

    task5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,25 +1,26 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
  * print_sign- print_alphabet
- *@n : the character
+ * @n : the character
  * Return: 0 or 1
  */
 int print_sign(int n)
 {
-if (n == 0)
-{
-_putchar('0');
-return (0);
-}
-else if (n != 0 && n > 0)
-{
-_putchar('+');
-return (1);
-}
-else if (n < 0)
-{
-_putchar('-');
-return (-1);
-}
-return (0);
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n != 0 && n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	return (0);
 }
