77e6b6180ae768482cb7672a36aa370e6b86f444
musteabdi <musteabdi@hotmail.com>
2022-12-28 05:24:09 -0800

    7 last degit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * print_last_digit- print_alphabet
  *@x : the character
@@ -6,16 +6,16 @@
  */
 int print_last_digit(int x)
 {
-int result;
+	int result;
 
-if (x >= 0)
-{
-result = x % 10;
-}
-else
-{
-result = (x % 10) * -1;
-}
-_putchar(result + '0');
-return (result);
+	if (x >= 0)
+	{
+		result = x % 10;
+	}
+	else
+	{
+		result = (x % 10) * -1;
+	}
+	_putchar(result + '0');
+	return (result);
 }
