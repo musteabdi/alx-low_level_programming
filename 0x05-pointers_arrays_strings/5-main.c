27be334f172538acdc96def15c40834357d1f462
musteabdi <musteabdi@hotmail.com>
2022-12-31 14:40:54 -0800

    task10

--- 0x05-pointers_arrays_strings/5-main.c
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -1,4 +1,4 @@
-#include "../main.h"
+#include "main.h"
 
 /**
  * main - check the code for ALX School students.
@@ -9,7 +9,7 @@
 int main(void)
 {
 	char s[10] = "My School";
-	char s2[11] = "Holberton!";
+	char s2[11] = "Alx Africa!";
 	char s3[446] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 
