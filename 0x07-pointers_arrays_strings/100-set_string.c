eee2031f7d7c34cafce286d4a0f64383e4256a64
musteabdi <musteabdi@hotmail.com>
2023-01-02 12:27:21 -0800

    task9

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - check the code for Holberton School students.
+ *@s : variable
+ *@to : variable
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
