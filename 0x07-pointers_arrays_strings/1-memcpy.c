84318c1e3c774ea3d7cb14a22b5bbfdedba932cd
musteabdi <musteabdi@hotmail.com>
2023-01-02 06:29:45 -0800

    task2

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,14 +7,14 @@
  * @n : the number of times we will fill the memory
  * Return: memory.
  */
-
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int i = 0;
-while (i < n)
-{
-dest[i] = src[i];
-i++;
-}
-return (dest);
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (-dest);
 }
